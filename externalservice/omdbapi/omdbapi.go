e78995bd71dd7f88a6222ffa823baf4428723293
Andre Febrianto <andrefebrianto88@gmail.com>
2021-10-25 18:51:29 +0700

    Update external service structure and method

--- externalservice/omdbapi/omdbapi.go
+++ externalservice/omdbapi/omdbapi.go
@@ -9,14 +9,20 @@ import (
 	"strconv"
 	"time"
 
+	"github.com/andrefebrianto/Search-Movie-Service/httpclient"
 	"github.com/andrefebrianto/Search-Movie-Service/movie"
 	"github.com/andrefebrianto/Search-Movie-Service/searchlog"
 )
 
-type MovieDataProvider struct {
-	BaseUrl   string `json:"baseUrl"`
-	ApiKey    string `json:"apiKey"`
-	SearchLog searchlog.SearchLogUseCase
+type OmdbConfig struct {
+	BaseUrl string `json:"baseUrl"`
+	ApiKey  string `json:"apiKey"`
+}
+
+type OmdbApi struct {
+	config     OmdbConfig
+	searchLog  searchlog.SearchLogUseCase
+	httpClient httpclient.HttpClient
 }
 
 type SearchResult struct {
@@ -32,19 +38,26 @@ type MovieDetailResult struct {
 	Error    string `json:"Error"`
 }
 
-func (dataProvider MovieDataProvider) SearchMovies(ctx context.Context, keyword string, page int) (*movie.MovieSearch, error) {
-	client := http.Client{}
-	request, err := http.NewRequestWithContext(ctx, "GET", dataProvider.BaseUrl, nil)
+func CreateOmdbApiClient(config OmdbConfig, searchLog searchlog.SearchLogUseCase, httpClient httpclient.HttpClient) OmdbApi {
+	return OmdbApi{
+		config:     config,
+		searchLog:  searchLog,
+		httpClient: httpClient,
+	}
+}
+
+func (dataProvider OmdbApi) SearchMovies(ctx context.Context, keyword string, page int) (*movie.MovieSearch, error) {
+	request, err := http.NewRequestWithContext(ctx, "GET", dataProvider.config.BaseUrl, nil)
 	if err != nil {
 		return nil, err
 	}
 	query := request.URL.Query()
-	query.Add("apikey", dataProvider.ApiKey)
+	query.Add("apikey", dataProvider.config.ApiKey)
 	query.Add("s", keyword)
 	query.Add("page", strconv.Itoa(page))
 	request.URL.RawQuery = query.Encode()
 
-	response, err := client.Do(request)
+	response, err := dataProvider.httpClient.Do(request)
 	if err != nil {
 		return nil, err
 	}
@@ -56,24 +69,10 @@ func (dataProvider MovieDataProvider) SearchMovies(ctx context.Context, keyword 
 
 	var responseData SearchResult
 	if err := json.NewDecoder(response.Body).Decode(&responseData); err != nil {
-		return nil, err
+		return nil, errors.New("invalid json format")
 	}
 
-	go func() {
-		contex, cancel := context.WithTimeout(context.Background(), time.Duration(2*time.Second))
-		defer cancel()
-		plainResponseData, _ := json.Marshal(responseData)
-		searchLog := searchlog.SearchLog{
-			Url:          request.RequestURI,
-			Status:       response.StatusCode,
-			ResponseData: string(plainResponseData),
-			Timestamp:    time.Now().Local(),
-		}
-		err := dataProvider.SearchLog.Create(contex, &searchLog)
-		if err != nil {
-			fmt.Println("Search Log Database Error: ", err)
-		}
-	}()
+	go dataProvider.StoreLog(request.URL.String(), response.StatusCode, responseData)
 
 	if responseData.Error == "Movie not found!" {
 		return nil, errors.New("not found")
@@ -90,18 +89,17 @@ func (dataProvider MovieDataProvider) SearchMovies(ctx context.Context, keyword 
 	return movieSearchResult, nil
 }
 
-func (dataProvider MovieDataProvider) GetMovieDetail(ctx context.Context, imdbId string) (*movie.Movie, error) {
-	client := http.Client{}
-	request, err := http.NewRequestWithContext(ctx, "GET", dataProvider.BaseUrl, nil)
+func (dataProvider OmdbApi) GetMovieDetail(ctx context.Context, imdbId string) (*movie.Movie, error) {
+	request, err := http.NewRequestWithContext(ctx, "GET", dataProvider.config.BaseUrl, nil)
 	if err != nil {
 		return nil, err
 	}
 	query := request.URL.Query()
-	query.Add("apikey", dataProvider.ApiKey)
+	query.Add("apikey", dataProvider.config.ApiKey)
 	query.Add("i", imdbId)
 	request.URL.RawQuery = query.Encode()
 
-	response, err := client.Do(request)
+	response, err := dataProvider.httpClient.Do(request)
 	if err != nil {
 		return nil, err
 	}
@@ -113,24 +111,10 @@ func (dataProvider MovieDataProvider) GetMovieDetail(ctx context.Context, imdbId
 
 	var responseData MovieDetailResult
 	if err := json.NewDecoder(response.Body).Decode(&responseData); err != nil {
-		return nil, err
+		return nil, errors.New("invalid json format")
 	}
 
-	go func() {
-		contex, cancel := context.WithTimeout(context.Background(), time.Duration(2*time.Second))
-		defer cancel()
-		plainResponseData, _ := json.Marshal(responseData)
-		searchLog := searchlog.SearchLog{
-			Url:          request.URL.String(),
-			Status:       response.StatusCode,
-			ResponseData: string(plainResponseData),
-			Timestamp:    time.Now().Local(),
-		}
-		err := dataProvider.SearchLog.Create(contex, &searchLog)
-		if err != nil {
-			fmt.Println("Search Log Database Error: ", err)
-		}
-	}()
+	go dataProvider.StoreLog(request.URL.String(), response.StatusCode, responseData)
 
 	if responseData.Error == "Incorrect IMDb ID." {
 		return nil, errors.New("not found")
@@ -141,4 +125,20 @@ func (dataProvider MovieDataProvider) GetMovieDetail(ctx context.Context, imdbId
 	movieDetail := responseData.Movie
 
 	return &movieDetail, nil
+}
+
+func (dataProvider OmdbApi) StoreLog(url string, statusCode int, responseData interface{}) {
+	contex, cancel := context.WithTimeout(context.Background(), time.Duration(2*time.Second))
+	defer cancel()
+	plainResponseData, _ := json.Marshal(responseData)
+	searchLog := searchlog.SearchLog{
+		Url:          url,
+		Status:       statusCode,
+		ResponseData: string(plainResponseData),
+		Timestamp:    time.Now().Local(),
+	}
+	err := dataProvider.searchLog.Create(contex, &searchLog)
+	if err != nil {
+		fmt.Println("Search Log Database Error: ", err)
+	}
 }
